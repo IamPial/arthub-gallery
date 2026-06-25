d82a1b38dddddd3ee2de713c320b6c637c34dce9
IamPial <pialname394@gmail.com>
2026-06-25 09:57:48 +0600

    pass token with headers

--- src/lib/actions/artworks.js
+++ src/lib/actions/artworks.js
@@ -1,12 +1,16 @@
 "use server";
 
+import { getTokenServer } from "../getToken";
+
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const addArtWorks = async (artWorks) => {
+  const token = await getTokenServer();
   const res = await fetch(`${baseURl}/api/artworks`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
+      authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(artWorks),
   });
