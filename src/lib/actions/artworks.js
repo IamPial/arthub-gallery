4a14cdebc265c94a3776c99b7389c844f7d4725d
IamPial <pialname394@gmail.com>
2026-06-25 16:42:43 +0600

    create a delete function for deleting any items

--- src/lib/actions/artworks.js
+++ src/lib/actions/artworks.js
@@ -4,6 +4,7 @@ import { getTokenServer } from "../getToken";
 
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
+//create artworks
 export const addArtWorks = async (artWorks) => {
   const token = await getTokenServer();
   const res = await fetch(`${baseURl}/api/artworks`, {
@@ -19,6 +20,7 @@ export const addArtWorks = async (artWorks) => {
   return data;
 };
 
+//edit artworks
 export const updateArtWorks = async (id, updateData) => {
   const token = await getTokenServer();
   const res = await fetch(`${baseURl}/api/artworks/${id}`, {
@@ -28,6 +30,20 @@ export const updateArtWorks = async (id, updateData) => {
       authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(updateData),
+  });
+  const data = await res.json();
+  return data;
+};
+
+//delete artworks
+export const deleteArtWorks = async (id) => {
+  const token = await getTokenServer();
+  const res = await fetch(`${baseURl}/api/artworks/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
   });
   const data = await res.json();
   return data;
