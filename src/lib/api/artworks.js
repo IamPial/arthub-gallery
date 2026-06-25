0145a436930a3aaa5515d84603f34f13d0ccbe5e
IamPial <pialname394@gmail.com>
2026-06-25 09:57:04 +0600

    pass the authorization headers

--- src/lib/api/artworks.js
+++ src/lib/api/artworks.js
@@ -1,9 +1,14 @@
-
+import { getTokenServer } from "../getToken";
 
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const getArtworks = async () => {
-  const res = await fetch(`${baseURl}/api/artworks`);
+  const token = await getTokenServer();
+  const res = await fetch(`${baseURl}/api/artworks`, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
   const data = await res.json();
   return data;
 };
