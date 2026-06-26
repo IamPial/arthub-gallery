77e4922889780bbfaf7f112f8bb75cd82fc02f23
IamPial <pialname394@gmail.com>
2026-06-26 14:26:34 +0600

    create get all artworks func

--- src/lib/api/artworks.js
+++ src/lib/api/artworks.js
@@ -2,6 +2,14 @@ import { getTokenServer } from "../getToken";
 
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
+//get all artworks
+export const getAllArtworks = async (search) => {
+  const res = await fetch(`${baseURl}/api/all-artworks?search=${search}`);
+  const data = await res.json();
+  return data;
+};
+
+//get artworks by artist
 export const getArtworks = async () => {
   const token = await getTokenServer();
   const res = await fetch(`${baseURl}/api/artworks`, {
