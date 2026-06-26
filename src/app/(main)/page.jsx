4a0b97d6ce694e6da8fddbb18e8e27a6c0d427ed
IamPial <pialname394@gmail.com>
2026-06-27 03:04:25 +0600

    render top artist sections

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,15 @@
+import ArtCategoriesSection from "@/components/homepage/ArtCategoriesSection";
 import Banner from "@/components/homepage/Banner";
 import FeaturedArtworksSection from "@/components/homepage/FeaturedArtworksSection";
+import TopArtistSection from "@/components/homepage/TopArtistSection";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <FeaturedArtworksSection />
+      <ArtCategoriesSection />
+      <TopArtistSection />
     </div>
   );
 };
