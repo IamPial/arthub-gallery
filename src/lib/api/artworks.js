5c74306472cd58b72402c68171c40761d78c159b
IamPial <pialname394@gmail.com>
2026-06-26 16:41:01 +0600

    created searh & filter functionality with getAllArtworks func

--- src/lib/api/artworks.js
+++ src/lib/api/artworks.js
@@ -3,8 +3,16 @@ import { getTokenServer } from "../getToken";
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 //get all artworks
-export const getAllArtworks = async (search) => {
-  const res = await fetch(`${baseURl}/api/all-artworks?search=${search}`);
+export const getAllArtworks = async (obj = {}) => {
+  const { search, minPrice, maxPrice, category } = obj;
+  const searchUrl = new URL(`${baseURl}/api/all-artworks`);
+
+  if (search) searchUrl.searchParams.set("search", search);
+  if (minPrice) searchUrl.searchParams.set("minPrice", minPrice);
+  if (maxPrice) searchUrl.searchParams.set("maxPrice", maxPrice);
+  if (category) searchUrl.searchParams.set("category", category);
+
+  const res = await fetch(searchUrl.toString());
   const data = await res.json();
   return data;
 };
