5b4aa59fdd5c0ce05029362d08ac42a3dbe9d00e
IamPial <pialname394@gmail.com>
2026-06-27 15:47:31 +0600

    render the purchase history with calling get Transactions func

--- src/app/dashboard/buyer/page.jsx
+++ src/app/dashboard/buyer/page.jsx
@@ -1,8 +1,14 @@
-"use client";
-
+import { getTransactions } from "@/lib/api/transactions";
 import { Table } from "@heroui/react";
 
-const PurchaseHistoryPage = () => {
+const PurchaseHistoryPage = async () => {
+  const purchaseData = await getTransactions();
+
+  // artistName: "Manik Hossain"
+  // price:"180"
+  // purchaseDate:"2026-06-27T09:17:04.123Z"
+  // title:"Golden Sunset Canvas"
+
   return (
     <div className="flex flex-col gap-6">
       <h2 className="text-xl font-bold tracking-wide text-white">
@@ -13,7 +19,10 @@ const PurchaseHistoryPage = () => {
         <Table.ScrollContainer>
           <Table.Content aria-label="Purchase history table">
             <Table.Header>
-              <Table.Column  isRowHeader className="bg-[#110d2c] text-slate-400 font-semibold py-4 border-b border-white/[0.04]">
+              <Table.Column
+                isRowHeader
+                className="bg-[#110d2c] text-slate-400 font-semibold py-4 border-b border-white/[0.04]"
+              >
                 ITEM LIST
               </Table.Column>
               <Table.Column className="bg-[#110d2c] text-slate-400 font-semibold py-4 border-b border-white/[0.04]">
@@ -28,50 +37,29 @@ const PurchaseHistoryPage = () => {
             </Table.Header>
 
             <Table.Body>
-              <Table.Row className="border-b border-white/[0.02]  hover:bg-white/[0.02] transition-colors">
-                <Table.Cell className="font-semibold bg-[#1d1932]  text-slate-400  py-4">
-                  Cute Cube Cool
-                </Table.Cell>
-                <Table.Cell className="text-slate-400 bg-[#1d1932] py-4">
-                  John Abraham
-                </Table.Cell>
-                <Table.Cell className="text-[#8b5cf6]  bg-[#1d1932] font-bold py-4">
-                  0.0025 ETH
-                </Table.Cell>
-                <Table.Cell className="text-slate-400  bg-[#1d1932] py-4">
-                  2026-05-12
-                </Table.Cell>
-              </Table.Row>
-
-              <Table.Row className="border-b border-white/[0.02] hover:bg-white/[0.02] transition-colors">
-                <Table.Cell className="font-semibold bg-[#1d1932] text-white py-4">
-                  Liquid Wave
-                </Table.Cell>
-                <Table.Cell className="text-slate-400 bg-[#1d1932] py-4">
-                  Alex Panda
-                </Table.Cell>
-                <Table.Cell className="text-[#8b5cf6] bg-[#1d1932] font-bold py-4">
-                  0.015 ETH
-                </Table.Cell>
-                <Table.Cell className="text-slate-400 bg-[#1d1932] py-4">
-                  2026-06-01
-                </Table.Cell>
-              </Table.Row>
-
-              <Table.Row className="border-b border-white/[0.02] hover:bg-white/[0.02] transition-colors">
-                <Table.Cell className="font-semibold bg-[#1d1932] text-white py-4">
-                  Abstract Passion
-                </Table.Cell>
-                <Table.Cell className="text-slate-400 bg-[#1d1932] py-4">
-                  David Miller
-                </Table.Cell>
-                <Table.Cell className="text-[#8b5cf6] bg-[#1d1932] font-bold py-4">
-                  0.0085 ETH
-                </Table.Cell>
-                <Table.Cell className="text-slate-400 bg-[#1d1932] py-4">
-                  2026-06-15
-                </Table.Cell>
-              </Table.Row>
+              {purchaseData.map((items) => {
+                return (
+                  <Table.Row
+                    key={items?._id}
+                    className="border-b border-white/[0.02]  hover:bg-white/[0.02] transition-colors"
+                  >
+                    <Table.Cell className="font-semibold bg-[#1d1932]  text-white  py-4">
+                      {items?.title}
+                    </Table.Cell>
+                    <Table.Cell className="text-slate-400 bg-[#1d1932] py-4">
+                      {items?.artistName}
+                    </Table.Cell>
+                    <Table.Cell className="text-[#8b5cf6]  bg-[#1d1932] font-bold py-4">
+                      ${items?.price}
+                    </Table.Cell>
+                    <Table.Cell className="text-slate-400  bg-[#1d1932] py-4">
+                      {new Date(items?.purchaseDate).toLocaleDateString(
+                        "en-US",
+                      )}
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
             </Table.Body>
           </Table.Content>
         </Table.ScrollContainer>
