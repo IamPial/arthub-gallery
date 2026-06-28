87e26860dfe091c413e9b9942e37de5770277a44
IamPial <pialname394@gmail.com>
2026-06-28 23:09:38 +0600

    intialize the role with query params for different dashboard users

--- src/lib/api/transactions.js
+++ src/lib/api/transactions.js
@@ -3,9 +3,9 @@ import { getTokenServer } from "../getToken";
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 //get all transactions
-export const getTransactions = async () => {
+export const getTransactions = async (role = "buyer") => {
   const token = await getTokenServer();
-  const res = await fetch(`${baseURl}/api/transactions`, {
+  const res = await fetch(`${baseURl}/api/transactions?role=${role}`, {
     headers: {
       authorization: `Bearer ${token}`,
     },
@@ -16,7 +16,6 @@ export const getTransactions = async () => {
 
 //get transactions details
 export const getTransactionsDetails = async (id) => {
-  const token = await getTokenServer();
   const res = await fetch(`${baseURl}/api/transactions/${id}`);
   const data = await res.json();
   return data;
