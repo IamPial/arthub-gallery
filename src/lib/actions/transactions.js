655f54393dfa6ad3e179755f2d65628715356899
IamPial <pialname394@gmail.com>
2026-06-29 18:32:16 +0600

    create delete api

--- src/lib/actions/transactions.js
+++ src/lib/actions/transactions.js
@@ -1,3 +1,7 @@
+"use server";
+
+import { getTokenServer } from "../getToken";
+
 const baseURl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 //create artworks
@@ -10,6 +14,20 @@ export const transactions = async (newData) => {
     body: JSON.stringify(newData),
   });
 
+  const data = await res.json();
+  return data;
+};
+
+//delete transactions
+export const deleteTransactions = async (id) => {
+  const token = await getTokenServer();
+  const res = await fetch(`${baseURl}/api/transactions/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  });
   const data = await res.json();
   return data;
 };
